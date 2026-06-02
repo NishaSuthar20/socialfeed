23fd7f47c83ca424df95bf8bb42524685890d251
NishaSuthar20 <nishasuthar2024@gmail.com>
2026-06-02 19:26:20 +0530

    Fix CORS allow all

--- backend/server.js
+++ backend/server.js
@@ -6,21 +6,16 @@ const path = require('path');
 
 const app = express();
 app.use(cors({
-  origin: [
-    // "http://localhost:5173",
-    "https://socialfeed-frontend.netlify.app"
-  ],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/posts', require('./routes/posts'));
 
-// MongoDB Connect
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB Connected ✅');
